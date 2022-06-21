3e279704af03ecc69cfb9af6b6652fc3cf813704
Joel Collyer <contact@joelcollyer.com>
2022-06-20 19:47:45 -0600

    Organizing

--- dateFormatting.js
+++
@@ -1,9 +0,0 @@
-/**
- * Putting a string into a new Date() makes a new date with no time.
- * What happens when you put a Date in a Date?
- */
-const dates = ["2022-03-29", new Date("2022-03-29")];
-
-const output = dates.map((date) => new Date(date));
-
-console.log(output); // Nothing, it's still a date with no time
