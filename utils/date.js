033bca7efd49fbc6a818030277b34c0d44a32731
Joel Collyer <contact@joelcollyer.com>
2022-06-24 15:06:00 -0600

    Utils - addDays and subtractDays

--- utils/date.js
+++ utils/date.js
@@ -8,6 +8,14 @@ const isBefore = (dateA, dateB) => dateB.valueOf() - dateA.valueOf() > 0;
 const isSameOrBefore = (dateA, dateB) =>
   isSameDay(dateA, dateB) || isBefore(dateA, dateB);
 
+const addDays = (date = new Date(), days = 1) => {
+  const nextDate = new Date(date);
+  nextDate.setDate(nextDate.getDate() + days);
+  return nextDate;
+};
+
+const subtractDays = (date = new Date(), days = 1) => addDays(date, -days);
+
 const dateA = new Date("2022-03-03 09:00:00");
 const dateB = new Date("2022-03-03 23:59:59");
 
@@ -15,4 +23,6 @@ console.log({
   isSameDay: isSameDay(dateA, dateB),
   isBefore: isBefore(dateA, dateB),
   isSameOrBefore: isSameOrBefore(dateA, dateB),
+  addDays: addDays("2022-03-29", 4),
+  subtractDays: subtractDays("2022-04-01", 3),
 });
