fb0674596d16b36622dddebdd3844477468ea2fa
Joel Collyer <contact@joelcollyer.com>
2022-07-21 08:45:12 -0600

    hideEmail: Using just .replace to obfuscate an email

--- interviewQuestions/hideEmail.js
+++ interviewQuestions/hideEmail.js
@@ -21,7 +21,20 @@ const hideEmail = (str = "", hideFull = false) => {
   return `${redact(local)}@${hideFull ? redact(domain) : domain}${tld}`;
 };
 
+// Attempt 2 - After discovering .replace() accepts a function as the 2nd param
+const hideEmailRegexp = (str = "", hideFull = false) => {
+  return str.replace(
+    /^.(.*?).@.(.*?)[^\.]((\.[a-z]+)+$)/i,
+    (email, local, domain) => {
+      let result = email;
+      result = result.replace(local, "*".repeat(local.length));
+      if (hideFull) result = result.replace(domain, "*".repeat(domain.length));
+      return result;
+    }
+  );
+};
+
 const hideFull = true;
-console.log(hideEmail("contact@joelcollyer.com")); // c*****t@joelcollyer.com
-console.log(hideEmail("contact@joel-collyer.co.uk", hideFull)); // c*****t@j*********r.co.uk
-console.log(hideEmail("it.support@somedomain.ninja", hideFull)); // i********t@s********n.ninja
+console.log(hideEmailRegexp("contact@joelcollyer.com")); // c*****t@joelcollyer.com
+console.log(hideEmailRegexp("contact@joel-collyer.co.uk", hideFull)); // c*****t@j*********r.co.uk
+console.log(hideEmailRegexp("it.support@somedomain.ninja", hideFull)); // i********t@s********n.ninja
