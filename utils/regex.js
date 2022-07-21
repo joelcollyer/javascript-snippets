c8e82e7d32482b03ab000207569754071c2a693d
Joel Collyer <contact@joelcollyer.com>
2022-07-21 08:45:00 -0600

    regex: Adding email validation/splitter expression

--- utils/regex.js
+++ utils/regex.js
@@ -2,3 +2,8 @@
 console.log(
   "http://test-api.lodgelink.com/".replace(/(^(http(s)?:\/\/)|(\/$))/g, "")
 );
+
+// Split an email into its localPart, domain, and tld
+console.log(
+  "test+account.123@example-domain.co.uk".match(/^(.*?)@(.*?)((\.[a-z]+)+$)/i)
+);
