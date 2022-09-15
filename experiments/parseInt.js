afb6c62601bc56a284b3d818a75816441994da5f
Joel Collyer <contact@joelcollyer.com>
2022-09-15 15:19:17 -0600

    Testing empty strings, null, and undefined in parseInt

--- experiments/parseInt.js
+++ experiments/parseInt.js
@@ -1,7 +1,19 @@
-const tests = [123, "123", "123.456", "1e10", "5E"];
+const tests = [
+  123,
+  "123",
+  "123.456",
+  "1e10",
+  "5E",
+  "0",
+  "",
+  false,
+  null,
+  undefined,
+];
 
 tests.forEach((int) => {
   console.log({
+    int,
     parseInt: parseInt(int, 10),
     coerce: +int,
     equality: +int == int,
