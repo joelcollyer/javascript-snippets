d1d22ca1cf5673f4b763ebe8dfdef41cb8931937
Joel Collyer <contact@joelcollyer.com>
2023-03-29 10:50:26 -0600

    Updating ttl to show time that has elapsed since a given start date

--- experiments/ttl.js
+++ experiments/ttl.js
@@ -1,11 +1,34 @@
-const secondsInAWeek = 86400 * 7;
+/**
+ * Challenge: Can I create a human readable string that communicates how much
+ * time has elapsed between two dates that are years apart?
+ *
+ * Known Issue: Leap years change the number of days/weeks in a year.
+ */
 
-const ttl = (date = new Date()) => {
+// The duration to compute relative time formats for
+const years = 90;
+const start = new Date("1987-03-29 00:00:00");
+const end = new Date().setFullYear(new Date(start).getFullYear() + years);
+
+// Constants
+const secondsInADay = 86400;
+const secondsInAWeek = secondsInADay * 7;
+
+// Convert a date to a relative time using Intl.RelativeTimeFormat
+const getRelativeTime = (date = new Date()) => {
   const milliseconds = new Date(date).getTime();
-  const secondsFromNow = Math.round((milliseconds - Date.now()) / 1000);
+  let secondsElapsed = Math.round((milliseconds - Date.now()) / 1000);
+  let duration = Math.floor(secondsElapsed / secondsInAWeek);
 
   const rtf = new Intl.RelativeTimeFormat("en-CA", { numeric: "auto" });
-  return rtf.format(Math.floor(secondsFromNow / secondsInAWeek), "week");
+  return rtf.format(duration, "week");
 };
 
-console.log(ttl("2077-03-29"));
+// Return a string that shows how many weeks have passed, and how many are left
+const ttl = () => {
+  const weeksAgo = getRelativeTime(start);
+  const weeksRemaining = getRelativeTime(end);
+  return `You started ${weeksAgo} and finish ${weeksRemaining}.`;
+};
+
+console.log(ttl());
