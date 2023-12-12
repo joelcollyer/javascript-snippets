adfb39d22c900213b510eced8ca74a475aae2501
Joel Collyer <contact@joelcollyer.com>
2023-12-12 00:22:37 -0700

    experiments/SevenSegment

--- experiments/SevenSegment.js
+++ experiments/SevenSegment.js
@@ -9,19 +9,20 @@ const SEGMENTS = {
   7: [1, 1, 1, 0, 0, 0, 0],
   8: [1, 1, 1, 1, 1, 1, 1],
   9: [1, 1, 1, 0, 0, 1, 1],
+  off: [0, 0, 0, 0, 0, 0, 0],
 };
 
 const EOL = "\r\n";
 
 const leftPad = (num = 0, len = 2, char = "0") => {
-  const str = !num ? "" : num.toString();
+  const str = num.toString();
   return str.length <= len ? `${char.repeat(len)}${str}`.slice(-len) : str;
 };
 
 const drawPixel = (isOn) => (isOn ? "🟥" : "⬛");
 
 const drawChar = (num = 0) => {
-  const seg = SEGMENTS[num];
+  const seg = SEGMENTS[num] || SEGMENTS["off"];
   const pos = [null, seg[0], null, seg[5], null, seg[1], null, seg[6], null, seg[4], null, seg[2], null, seg[3], null];
   return pos.map((on, i) => ((i + 1) % 3 === 0 ? drawPixel(on) + EOL : drawPixel(on))).join("");
 };
@@ -31,4 +32,14 @@ const drawNum = (num = 0, len = 2) => {
   return [...leftPad(num, len)].map((num) => drawChar(+num)).join("");
 };
 
-console.log(drawNum(8, 2));
+console.log(drawNum(0, 0));
+console.log(drawNum(1, 0));
+console.log(drawNum(2, 0));
+console.log(drawNum(3, 0));
+console.log(drawNum(4, 0));
+console.log(drawNum(5, 0));
+console.log(drawNum(6, 0));
+console.log(drawNum(7, 0));
+console.log(drawNum(8, 0));
+console.log(drawNum(9, 0));
+console.log(drawNum("x", 0));
