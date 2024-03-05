6c63e7c8892f673342d97b75f0207301a9e4ae09
Joel Collyer <contact@joelcollyer.com>
2024-03-05 09:01:00 -0700

    Adding an isNumeric helper for finding zeroes or number strings

--- experiments/isNaN.js
+++ experiments/isNaN.js
@@ -1,17 +1,16 @@
-const tests = [
-  0,
-  -1,
-  123,
-  123.456,
-  "0",
-  null,
-  undefined,
-  "123.4356",
-  NaN,
-  Number.MAX_SAFE_INTEGER,
-];
+const tests = [0, -1, 123, 123.456, "0", null, undefined, "123.4356", NaN, Number.MAX_SAFE_INTEGER];
 
 tests.forEach((num) => console.log({ [num]: isNaN(num) }));
 
 const filterOnNumber = tests.filter(Number);
-console.log({ filterOnNumber });
+console.log({ filterOnNumber }, "\r\n");
+
+/**
+ * A helper function is needed to filter falsey non-numerics out:
+ */
+const isNumeric = (num) => {
+  if (num === null) return false;
+  const parsed = +num;
+  return parsed === 0 || !isNaN(parsed);
+};
+tests.forEach((num) => console.log({ [num]: isNumeric(num) }));
