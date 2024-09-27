72e67d372711dbbfc43ac4e03a31712b5d749cb3
Joel Collyer <contact@joelcollyer.com>
2024-09-27 12:24:37 -0600

    fix(csvToJSON): Parse numbers and dates when creating objects

--- utils/csvToJSON.js
+++ utils/csvToJSON.js
@@ -2,10 +2,17 @@ const path = require("path");
 const { readFile, writeFile } = require("./fileManager");
 const { parseCSV } = require("./csv");
 
-const ROOT_PATH = __dirname;
-const INPUT_FILE_NAME = "input.csv";
-const OUTPUT_FILE_NAME = "output.json";
-const COLUMNS = ["bookingId", "guestId", "guestDayId", "roomIndex", "date", "guestName", "state"];
+
+// Define the data type for each column in the CSV
+const COLUMNS = {
+  bookingId: Number,
+  guestId: Number,
+  guestDayId: Number,
+  roomIndex: Number,
+  date: Date,
+  guestName: String,
+  state: String,
+};
 
 async function convertCSVtoJSON() {
   const inputFilePath = path.resolve(ROOT_PATH, INPUT_FILE_NAME);
@@ -13,16 +20,23 @@ async function convertCSVtoJSON() {
 
   const csv = parseCSV(data);
 
-  // Validate that every parsed csv row has a value for the desired columns
-  csv.forEach((row, rowNumber) =>
-    Object.entries(row).every(([column, value]) => {
-      if (!COLUMNS.includes(column)) throw new Error(`Row ${rowNumber + 2} is missing the ${column} column.`);
-      if (!value?.length) throw new Error(`Row ${rowNumber + 2} has no data in the ${column} column.`);
-      return true;
-    })
+  // Validate that every parsed csv row has a value for the desired columns, and format the value
+  const columnNames = Object.keys(COLUMNS);
+  const formattedValues = csv.map((row, rowNumber) =>
+    Object.fromEntries(
+      Object.entries(row).map(([column, value]) => {
+        // Reject invalid rows
+        if (!columnNames.includes(column)) throw new Error(`Row ${rowNumber + 2} is missing the ${column} column.`);
+        if (!value?.length) throw new Error(`Row ${rowNumber + 2} has no data in the ${column} column.`);
+
+        // Pass the value through a constructor so that it's formatted correctly (e.g. convert a string value to a Number)
+        const type = COLUMNS[column];
+        return [column, new type(value)];
+      })
+    )
   );
 
-  const json = JSON.stringify(csv);
+  const json = JSON.stringify(formattedValues);
 
   const outputFilePath = path.resolve(ROOT_PATH, OUTPUT_FILE_NAME);
   await writeFile(outputFilePath, json);
