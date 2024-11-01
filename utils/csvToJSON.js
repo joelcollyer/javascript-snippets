9f2b0ed41c1fa3b101ce421b0f590b37b13332ff
Joel Collyer <contact@joelcollyer.com>
2024-11-01 10:45:22 -0600

    fix(utils/csvToJSON): Group parsed data together

--- utils/csvToJSON.js
+++ utils/csvToJSON.js
@@ -2,6 +2,10 @@ const path = require("path");
 const { readFile, writeFile } = require("./fileManager");
 const { parseCSV } = require("./csv");
 
+const ROOT_PATH = "\\";
+const INPUT_FILE_NAME = "source_file_name.csv";
+const OUTPUT_FILE_NAME = "output_file_name.json";
+
 class ISODate extends Date {
   regexISODate = new RegExp(/^\d{4}-\d{2}-\d{2}$/);
 
@@ -45,32 +49,40 @@ async function convertCSVtoJSON() {
 
   // Validate that every parsed csv row has a value for the desired columns, and format the value
   const columnNames = Object.keys(COLUMNS);
-  const formattedValues = csv.map((row, rowNumber) =>
-    Object.fromEntries(
-      Object.entries(row)
-        .map(([column, value]) => {
-          // Skip excluded columns
-          if (REMOVE_COLUMNS.includes(column)) return [];
+  const formattedRows = csv
+    .map((row, rowNumber) =>
+      Object.fromEntries(
+        Object.entries(row)
+          .map(([column, value]) => {
+            // Skip excluded columns
+            if (REMOVE_COLUMNS.includes(column)) return [];
 
-          // Reject invalid rows
-          if (!columnNames.includes(column)) {
-            throw new Error(`Row ${rowNumber + 2} is missing the ${column} column.`);
-          }
+            // Reject invalid rows
+            if (!columnNames.includes(column)) {
+              throw new Error(`Row ${rowNumber + 2} is missing the ${column} column.`);
+            }
 
-          // Reject missind data
-          if (!value?.length && !OPTIONAL_COLUMNS.includes(column)) {
-            throw new Error(`Row ${rowNumber + 2} has no data in the ${column} column.`);
-          }
+            // Reject missind data
+            if (!value?.length && !OPTIONAL_COLUMNS.includes(column)) {
+              throw new Error(`Row ${rowNumber + 2} has no data in the ${column} column.`);
+            }
 
-          // Pass the value through a constructor so that it's formatted correctly (e.g. convert a string value to a Number)
-          const Type = COLUMNS[column];
-          return [column, new Type(value)];
-        })
-        .filter((row) => row?.length > 1) // Remove empty arrays
+            // Pass the value through a constructor so that it's formatted correctly (e.g. convert a string value to a Number)
+            const Type = COLUMNS[column];
+            return [column, new Type(value)];
+          })
+          .filter((row) => row?.length > 1) // Remove empty arrays
+      )
     )
-  );
+    .reduce((grouped, row) => {
+      // Group results by bookingId
+      const { bookingId } = row;
+      if (!grouped[bookingId]?.length) grouped[bookingId] = [];
+      grouped[bookingId].push(row);
+      return grouped;
+    }, {});
 
-  const json = JSON.stringify(formattedValues);
+  const json = JSON.stringify(formattedRows);
 
   const outputFilePath = path.resolve(ROOT_PATH, OUTPUT_FILE_NAME);
   await writeFile(outputFilePath, json);
